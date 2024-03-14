4ffba50f89b7444af97b6ced90a0b4f9db6c2612
chrischan8551 <chrischan8551@gmail.com>
2024-03-13 23:27:26 -0500

    init

---
+++ src/main/java/learn/App.java
@@ -0,0 +1,22 @@
+package learn;
+
+import learn.ui.Controller;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@ComponentScan
+@PropertySource("classpath:data.properties")
+public class App {
+    public static void main(String[] args) {
+        configureWithAnnotationAndRun();
+    }
+
+    //Annotation Dependency Injection
+    public static void configureWithAnnotationAndRun() {
+        ApplicationContext container = new AnnotationConfigApplicationContext(App.class);
+        Controller controller = container.getBean(Controller.class);
+        controller.run();
+    }
+}
---
+++ src/main/java/learn/ui/Controller.java
@@ -0,0 +1,12 @@
+package learn.ui;
+
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class Controller {
+    public void run() {
+
+    }
+}
